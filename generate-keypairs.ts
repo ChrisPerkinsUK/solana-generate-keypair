b9fabe216b716d302bf0f84fc2ed5f1caacad424
= <9754313+ChrisPerkinsUK@users.noreply.github.com>
2025-01-02 12:24:06 +0100

    Changed ./geneerate-kepair.ts to load secret key from .env file

--- generate-keypairs.ts
+++ generate-keypairs.ts
@@ -1,9 +1,9 @@
-import { Keypair } from "@solana/web3.js";
+import "dotenv/config";
 
-const keypair = Keypair.generate();
+import { getKeypairFromEnvironment } from "@solana-developers/helpers";
 
-console.log(`The public key is: ` + keypair.publicKey.toBase58());
+const keypair = getKeypairFromEnvironment("SECRET_KEY");
 
-console.log(`The private key is: ` + keypair.secretKey);
-
-console.log(`Finished!`);
+console.log(
+    `Finished! We've loaded our secret key securely using a .env file!`,
+);
